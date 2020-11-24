3b321d640dc542423236f42e13a79f418380eb65
faurekevin <faurekevin333@gmail.com>
2020-11-24 13:49:23 +0100

    etape5 avec 1 erreur ts

--- dist/script.js
+++ dist/script.js
@@ -3,3 +3,22 @@ function initApp() {
     console.log("bonjour");
 }
 initApp();
+var tabMam = [];
+function addMam(ordre, nom, image, paragraphe) {
+    tabMam.push({
+        ordre: ordre,
+        nom: nom,
+        image: image,
+        paragraphe: paragraphe
+    });
+    var mam = document.getElementById("mami");
+    //generation du contenu html
+    mam.innerHTML += "\n\t\t<div>\n\t\t\t<h2>" + ordre + "</h2>\n\t\t\t<h3>" + nom + "</h3>\n\t\t\t<img src=\"" + image + "\" title=\"" + nom + "\" alt=\"" + nom + "\">\n\t\t\t<p>" + paragraphe + "</p>\n\t\t</div>\n\t";
+}
+;
+addMam("Félin", "Chat", "../images/chat-1.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam("Cannidé", "Loup", "../images/loup1.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam("Primate", "Ouistiti", "../images/ouistiti.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam("Marsupial", "Koala", "../images/koala.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam("Herpestidae", "Suricat", "../images/surricat1.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+console.table(tabMam);
--- src/script.ts
+++ src/script.ts
@@ -2,3 +2,47 @@ function initApp() :void {
 	console.log("bonjour");
 }
 initApp();
+/*
+// tableau
+let mamArray = [];
+//generation du tableau
+function newMam(){
+
+};
+*/
+type mammifere = {
+	ordre:string;
+	nom:string;
+	image:string;
+	paragraphe:string;
+};
+
+let tabMam : mammifere[] = [];
+
+function addMam(ordre:string, nom:string, image:string , paragraphe:string , /* tabMam:mammifere [] */) {
+	tabMam.push({
+		ordre: ordre,
+		nom: nom,
+		image: image,
+		paragraphe: paragraphe
+	});
+	let mam=document.getElementById("mami");
+
+//generation du contenu html
+mam.innerHTML+=`
+		<div>
+			<h2>${ordre}</h2>
+			<h3>${nom}</h3>
+			<img src="${image}" title="${nom}" alt="${nom}">
+			<p>${paragraphe}</p>
+		</div>
+	`;
+};
+
+addMam ("Félin", "Chat", "../images/chat-1.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam ("Cannidé", "Loup", "../images/loup1.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam ("Primate", "Ouistiti", "../images/ouistiti.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam ("Marsupial", "Koala", "../images/koala.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+addMam ("Herpestidae", "Suricat", "../images/surricat1.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, inventore!");
+
+console.table(tabMam);
